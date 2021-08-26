78ce276e8837f454bcf74ad1dd6b6483eb747428
LAPTOP-7ORUAPFA\myoung.e <mythe1004@naver.com>
2021-08-26 21:47:17 +0900

    21-08-26 config, logback 설정 및 확인

--- src/main/java/com/example/SpringExampleApplication.java
+++ src/main/java/com/example/SpringExampleApplication.java
@@ -10,10 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@MapperScan(basePackages = "com.example.*")  // mapper scan 추가!!!
-
 // @ComponentScan(basePackages={"com", "com.example","com.myoung"})
 // @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) => DB연동해야하기 때문에 주석처리!
+
 @SpringBootApplication
 public class SpringExampleApplication {
 
@@ -24,15 +23,5 @@ public class SpringExampleApplication {
 	 * @ComponentScan(basePackages={"com", "com.example", ...etc}) => basePackage가 아닌 패키지에 spring bean을 사용하고 싶을 때 쓰는 어노테이션 
 	 *    ㄴ component
 	 * */
-	
-	@Bean // 스프링객체
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
 
-        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-        sessionFactory.setMapperLocations(res);
-
-        return sessionFactory.getObject();
-    }
 }
---
+++ src/main/java/com/example/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.example.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+// DB 설정(config)
+
+@MapperScan(basePackages = "com.example.*")  // mapper scan 추가!!!
+@Configuration // 설정임을 알려주는 어노테이션
+public class DatabaseConfig {
+	
+	@Bean // 스프링객체
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+}
